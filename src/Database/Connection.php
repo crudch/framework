a79bf142e7cec6f685f25f9f32ddb083d38e224b
Skip <skiphog@gmail.com>
2020-07-15 11:11:08 +0300

    change connections

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -8,16 +8,41 @@ class Connection extends PDO
 {
     public function __construct(string $dsn, array $config)
     {
-        parent::__construct(
-            "mysql:dbname={$config['dbname']};host={$config['host']};charset=utf8mb4",
-            $config['username'],
-            $config['password'],
-            $config['options']
-        );
+        parent::__construct($dsn, $config['username'], $config['password'], $config['options']);
     }
 
     public static function connect()
     {
+        $config = config('db');
 
+        return static::{$config['driver']}($config[$config['driver']]);
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return Connection
+     */
+    protected static function mysql(array $config): Connection
+    {
+        $dsn = "mysql:dbname={$config['database']};host={$config['host']};charset={$config['charset']}";
+
+        return new static($dsn, $config);
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return Connection
+     */
+    protected static function sqlite(array $config): Connection
+    {
+        $dsn = "sqlite:{$config['database']}";
+
+        foreach (['username', 'password', 'options'] as $item) {
+            $config[$item] = null;
+        }
+
+        return new static($dsn, $config);
     }
 }
